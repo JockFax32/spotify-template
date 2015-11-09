8fb4efd3ca26a95890a5b45fe49cb053061dc400
unknown <foxtjack@gmail.com>
2015-11-08 16:16:07 -0800

    Related Genre

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var dataArtist;
 var artistList = [];
 var baseUrl = 'https://api.spotify.com/v1/search?type=artist&query='
 var getUrl = 'https://api.spotify.com/v1/artists/'
-var echoUrl = 'http://developer.echonest.com/api/v4/artist/biographies?api_key=ADKYSLYABB1EDPIEE'
+var echoUrl = 'http://developer.echonest.com/api/v4/artist/profile?api_key=ADKYSLYABB1EDPIEE&id=spotify:artist:'
+
 
 
 var myApp = angular.module('myApp', ['ui.bootstrap']);
@@ -16,13 +17,17 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
   }
     $http.get(baseUrl + $scope.artist).success(function(response){
       dataArtist = $scope.artists = response.artists.items
+      //Update Breadcrumb footer based on searches
       artistList.push($scope.artist);
       $scope.list=artistList;
-      // User needs to visual see that the page has updated after selecting
-      // an artist from within the suggestions
+      
+      /*User needs to visual see that the page has updated after selecting
+       an artist from within the suggestions */
       getSongs();
       getRelated();
       getAlbums();
+      getEcho('0LbLWjaweRbO4FDKYlbfNt');
+
     })
   }
   var getRelated =function(){
@@ -37,17 +42,20 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
 
     })
   }
-
   var getAlbums = function (){
     $http.get(getUrl+dataArtist[0].id+'/albums?market=US&album_type=album').success(function(response){
        $scope.artistAlubm = response['items'];
-
     })
   }
   $scope.getAlbumTracks = function(albumId){
     $http.get('https://api.spotify.com/v1/albums/'+ albumId +'/tracks').success(function(response){
       $scope.albtracks = response['itmes'];
       $scope.isCollapsed = true;
+    })
+  }
+ var getEcho = function (id){
+    $http.get(echoUrl+id+'&bucket=genre&format=json').success(function(response){
+      $scope.genrelists= response.response.artist.genres
     })
   }
   $scope.play = function(song) {
